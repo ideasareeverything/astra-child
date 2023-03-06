98443bd93aa5cb58802bf8d17c22ab67ef6ba5ca
Ben Llewellyn <124093982+ideasareeverything@users.noreply.github.com>
2023-03-06 11:10:09 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -55,3 +55,15 @@ if($cookie == "accepted")
 	}
 	add_action('wp_head', 'gtm_html', 80);
 }
+
+
+/* Siteground Auto Updates ************************************************************************
+*************************************************************************************************** */
+
+if(file_exists("/var/lib/sec/wp-settings.php")) 
+{        
+    define('WP_AUTO_UPDATE_CORE', true);
+    define( 'AUTOMATIC_UPDATER_DISABLED', false );
+    add_filter('auto_update_plugin', '__return_true');
+    add_filter('auto_update_theme', '__return_false');
+}
